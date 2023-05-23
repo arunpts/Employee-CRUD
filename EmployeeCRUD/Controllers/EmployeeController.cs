319b5feba32c466fb6de682b40ced68ec09e58d2
Arun Prakash <57718711+arunpts@users.noreply.github.com>
2023-05-23 13:43:52 +0530

    Add project files.

---
+++ EmployeeCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,102 @@
+﻿using EmployeeCRUD.Data;
+using EmployeeCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Employee> objCatlist = _context.Employees;
+            return View(objCatlist);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Employee empobj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(empobj);
+                _context.SaveChanges();
+                TempData["ResultOk"] = "Record Added Successfully !";
+                return RedirectToAction("Index");
+            }
+
+            return View(empobj);
+        }
+        //EDIT GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var empfromdb = _context.Employees.Find(id);
+
+            if (empfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(empfromdb);
+        }
+        //EDIT POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Employee empobj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Update(empobj);
+                _context.SaveChanges();
+                TempData["ResultOk"] = "Data Updated Successfully !";
+                return RedirectToAction("Index");
+            }
+
+            return View(empobj);
+        }
+        //DELETE GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var empfromdb = _context.Employees.Find(id);
+
+            if (empfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(empfromdb);
+        }
+        //DELETE POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteEmp(int? id)
+        {
+            var deleterecord = _context.Employees.Find(id);
+            if (deleterecord == null)
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(deleterecord);
+            _context.SaveChanges();
+            TempData["ResultOk"] = "Data Deleted Successfully !";
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
